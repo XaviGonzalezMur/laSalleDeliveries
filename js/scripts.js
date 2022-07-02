e7a7ca891590fbb92615891d592edb807a79fd81
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-07-02 14:55:50 +0200

    Adds a border when mouseover on any div and removes it when out, only when alt key is pressed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 window.addEventListener("load", () => {
     window.alert(new Date().toLocaleTimeString());
 });
+
+window.addEventListener("load", () => {
+    const divs = document.getElementsByTagName('div');
+
+    for(let div of divs) {
+        div.addEventListener('mouseover', (ev) => {
+            const target = ev.target;
+            if(ev.altKey) {
+                target.classList.add("div_highlighted");
+            }
+        });
+
+        div.addEventListener('mouseout', (ev) => {
+            const target = ev.target;
+            if(ev.altKey) {
+                target.classList.remove("div_highlighted");
+            }
+        });
+    };
+});
