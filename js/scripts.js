a0695ab0c1ca3ddf1f64259f68efc3424ef3a2e2
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-07-08 20:59:52 +0200

    New version to simulate a click on the navigation bar links when clicking on numbers from 1 to 5.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 window.addEventListener("load", () => {
-    window.alert(new Date().toLocaleTimeString());
+  window.alert(new Date().toLocaleTimeString());
 });
 
 window.addEventListener("load", () => {
-    const divs = document.getElementsByTagName('div');
+  const divs = document.getElementsByTagName('div');
 
-    for(let div of divs) {
-        div.addEventListener('mouseover', (event) => {
-            const target = event.target;
-            if(event.altKey) {
-                target.classList.add("div_highlighted");
-            }
-        });
+  for (let div of divs) {
+    div.addEventListener('mouseover', (event) => {
+      const target = event.target;
+      if (event.altKey) {
+        target.classList.add("div_highlighted");
+      }
+    });
 
-        div.addEventListener('mouseout', (event) => {
-            const target = event.target;
-            if(event.altKey) {
-                target.classList.remove("div_highlighted");
-            }
-        });
-    };
+    div.addEventListener('mouseout', (event) => {
+      const target = event.target;
+      if (event.altKey) {
+        target.classList.remove("div_highlighted");
+      }
+    });
+  };
 });
 
 // Section scrolling version 1
@@ -45,3 +45,17 @@ window.addEventListener("load", () => {
 //       }
 //   }, false);
 
+// Section scrolling version 2
+(function addScrollNavigationWithKeysVersion2() {
+  const navBarLinks = document.getElementById("navigation_bar").getElementsByClassName("nav-link");
+
+  [...navBarLinks].forEach((element, i) => {
+    document.addEventListener('keyup', (event) => {
+      var code = event.code.slice(-1) - 1; // from 0 to 4
+
+      if (code == i) {
+        element.click();
+      }
+    });
+  })
+})();
