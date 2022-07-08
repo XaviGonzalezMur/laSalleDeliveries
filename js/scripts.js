6e21bf61623519164cbdde1d32c70d518c89e51d
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-07-09 00:48:35 +0200

    Improvements on old code: - Added every functionality inside a method for a better encapsulation. Then, they are executed once. - Added detailed comments for a better comprehension. - Minor optimizations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,63 @@
-window.addEventListener("load", () => {
-  window.alert(new Date().toLocaleTimeString());
-});
+(function showLocalTimeWhenPageLoaded() {
+  // When the window is loaded, show an alert window with the local time
+  window.addEventListener("load", () => {
+    window.alert(new Date().toLocaleTimeString());
+  });
+})();
 
-window.addEventListener("load", () => {
-  const divs = document.getElementsByTagName('div');
+(function showDivsWithBordersOnMouseOverAndAltKeyPressed() {
+  // When the window is loaded...
+  window.addEventListener("load", () => {
+    const divs = document.getElementsByTagName('div');
 
-  for (let div of divs) {
-    div.addEventListener('mouseover', (event) => {
-      const target = event.target;
-      if (event.altKey) {
-        target.classList.add("div_highlighted");
-      }
-    });
+    // ...add to every 'div', two listeners:
+    for (let div of divs) {
+      // When mouse is over...
+      div.addEventListener('mouseover', (event) => {
+        // ...and Alt key is pressed, a new CSS class is added to highlight the div (target)
+        if (event.altKey) {
+          // This CSS class must be defined in 'main-styles.css' file
+          event.target.classList.add("div_highlighted");
+        }
+      });
 
-    div.addEventListener('mouseout', (event) => {
-      const target = event.target;
-      if (event.altKey) {
-        target.classList.remove("div_highlighted");
-      }
-    });
-  };
-});
+      // When mouse is out...
+      div.addEventListener('mouseout', (event) => {
+        // ...and Alt key is pressed, the CSS class is removed (so it's disabled)
+        if (event.altKey) {
+          event.target.classList.remove("div_highlighted");
+        }
+      });
+    };
+  });
+})();
 
-// Section scrolling version 1
-// document.addEventListener('keydown', (event) => {
-//     var code = event.code;
+// /* Section scrolling version 1.
+//    Notice this code has all the combinations link-key hardcoded, so it's not change-proof. */
+// (function addScrollNavigationWithKeysVersion1() {
+//   // Adding just one listener to process all possible sections to scroll, when a key is down
+//   document.addEventListener('keydown', (event) => {
+//     let code = event.code;
 
 //     switch (code) {
-//         case "Digit1":
-//           window.location = "#who_we_are";
-//           break;
-//         case "Digit2":
-//           window.location = "#services";
-//           break;
-//         case "Digit3":
-//           window.location = "#portfolio";
-//           break;
-//         case "Digit4":
-//           window.location = "#Location";
-//           break;
-//         case "Digit5":
-//           window.location = "#email_us";
-//           break;
-//       }
+//       case "Digit1":
+//         window.location = "#who_we_are";
+//         break;
+//       case "Digit2":
+//         window.location = "#services";
+//         break;
+//       case "Digit3":
+//         window.location = "#portfolio";
+//         break;
+//       case "Digit4":
+//         window.location = "#Location";
+//         break;
+//       case "Digit5":
+//         window.location = "#email_us";
+//         break;
+//     }
 //   }, false);
+// })();
 
 // Section scrolling version 2
 (function addScrollNavigationWithKeysVersion2() {
