85d74c6a28faf34f0876c799470e41de15572a0e
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-07-16 13:10:42 +0200

    Advanced JS exercise of page 31: - Used entries() function. - Extra: Instead of working with an HTMLCollection, called querySelectorAll to have a NodeList, avoiding the conversion of navBarLinks to an Array.

--- js/navigateSections.js
+++ js/navigateSections.js
@@ -1,16 +1,14 @@
-  // An HTMLCollection that contains all anchors (links) of the navigation bar
-  const navBarLinks = document.getElementById("navigation_bar").getElementsByClassName("nav-link");
+// A NodeList that contains all anchors (links) of the navigation bar
+const navBarLinks = document.getElementById("navigation_bar").querySelectorAll(".nav-link");
 
-  // Conversion to Array with 3 dots so that 'forEach' can be used.
-  // Array.from(navBarLinks) is an alternative.
-  [...navBarLinks].forEach((link, index) => {
+for (let [index, link] of navBarLinks.entries()) {
     // Adding as many listeners as items the navigation bar has, to react on 'keyup' event from keyboard.
     // The next lambda creates a Closure scope where 'link' and 'index' will have fixed values,
     // so every listener is configured for a unique combination of link and key(=index).
     document.addEventListener('keyup', (event) => {
-      // The link is clicked when the code of the pressed key matches the proper link (i.e. its index)
-      if (Number(event.key) === index + 1) {
-        link.click();
-      }
+        // The link is clicked when the code of the pressed key matches the proper link (i.e. its index)
+        if (Number(event.key) === index + 1) {
+            link.click();
+        }
     });
-  });
+};
