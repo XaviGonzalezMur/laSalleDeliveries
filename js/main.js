7d5cd8fdcdb5c7144686aeeaf0cbcc2327edea4c
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-07-21 23:57:25 +0200

    Revert "Stimulus framework usage:"
    This reverts commit 9926da16012f283d4c42d024dd35b25bb2aeca1b.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,3 @@ import "./showDate.js";
 import "./highlightDivs.js";
 import "./navigateSections.js";
 import "./playfulPortfolio.js"
-
-import { Application } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
-import { KeynavController } from "./keynav_controller.js"
-
-const app = Application.start();
-app.register("kn", KeynavController);
